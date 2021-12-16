5c3125fb26b42998c0f5ed0ed4dbebbc4b3b9cc0
Ubaid Siddiqui <ubdsiddiqui404@gmail.com>
2021-12-16 19:12:04 +0530

    initial commit.

---
+++ FactorialExample.java
@@ -0,0 +1,25 @@
+package dsapractice;
+public class FactorialExample {
+
+	public static void main(String[] args) throws StackOverflowError {
+		// TODO Auto-generated method stub
+		int n = 4;
+		
+		int result = factorial(n);
+		
+		System.out.println("factorial of " + n + "is : " + result);
+		
+	}
+	
+	public static int factorial(int n) {
+		
+		if(n == 1) {
+			return 1;
+		}
+			
+		else {
+			return n * factorial(n-1);
+		}
+	}
+
+}
